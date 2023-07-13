3d4db37ebad8f4b94c7c679e7be99a56d2be3334
realalfair <irina.merzdorf@gmail.com>
2023-07-13 12:26:23 +0200

    Update

--- script_01.js
+++ script_01.js
@@ -15,3 +15,28 @@
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
+// let firstName; // Deklaration (Definition)
+// firstName = "Max"; // Wertzuweisung
+// console.log(firstName); // Ausgabe
+
+// let familyName;
+// familyName = "Mütze";
+// console.log(familyName);
+
+/***** 03 Deklaration + Wertzuweisung II *******/
+
+// let firstName, familyName;
+
+// firstName = prompt("Bitte Vornamen eingeben:");
+// familyName = prompt("Bitte Nachnamen eingeben:");
+
+// console.log(firstName + " " + familyName);
+
+let test;
+test = "Maxine";
+test = 2;
+test = false;
+
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+
